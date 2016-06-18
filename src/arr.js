62c2a0e06c843f30f0f1566e7c6be74386f391d2
cstuncsik <csaba.tuncsik@gmail.com>
2016-06-18 09:01:14 +0200

    fix(array shuffle):

--- src/arr.js
+++ src/arr.js
@@ -2,9 +2,15 @@ import mat from './mat';
 export default (() => {
 
     const pickRand = arr => arr[mat.rand.int(0, arr.length-1)];
-    const shuffle = arr => arr.slice().reverse().map((v, i, a) => a[mat.rand.int(0, i)]);
     const range = (start, end) => Array.apply(null, Array(end - start + 1)).map((n, i) => i + start);
+    const shuffle = arr => arr.slice().reverse().reduce((p, c, i, a) => {
+        let rnd = mat.rand.int(0, i);
+        let tmp = a[i];
+        a[i] = a[rnd];
+        a[rnd] = tmp;
+        return a;
+    }, []);
 
-    return {pickRand, shuffle, range};
+    return {pickRand, range, shuffle};
 
 })();
