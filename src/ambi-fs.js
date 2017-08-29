cc2a7b1bd92891423702502d5127cee0a767449e
Tom Rogers <tom@roguevendor.com>
2017-08-29 00:36:47 -0500

    removed unnecessary ambi-fs code

--- src/ambi-fs.js
+++ src/ambi-fs.js
@@ -8,6 +8,7 @@ export const readFile = (path, options) => {
 	else return FS.readFileSync(path, fsOpts)
 }
 
+
 export const writeFile = (path, string, options) => {
 	let { async, ...fsOpts }  = options
 
@@ -15,21 +16,10 @@ export const writeFile = (path, string, options) => {
 	else return FS.writeFileSync(path, string, fsOpts)
 }
 
+
 export const mkdir = (path, options) => {
 	let { async, ...fsOpts } = options
 
 	if(async) return new Promise((resolve, reject) => FS.mkdir(path, fsOpts, (error) => error ? reject(error) : resolve()))
 	else return FS.mkdirSync(path, fsOpts)
-}
-
-export const access = (path, mode, options) => {
-	let { async, ...fsOpts } = options
-
-	if(async) return new Promise((resolve, reject) => FS.access(path, mode, (error) => error ? reject(error) : resolve(true)))
-	else return FS.accessSync(path, mode)
-}
-
-export const exists = (path, options) => {
-	// based on stuff I've read, checking for read+write permission sounds like better method for existence check
-	return access(path, FS.constants.R_OK | FS.constants.W_OK, options)
 }
